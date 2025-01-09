a7d0dd679cc4ce109ce898aab20153cbb6a35bfb
Henrik <106661204+henrikbreimark@users.noreply.github.com>
2025-01-09 21:24:50 +0100

    Added function that generates a random computer choice

--- gameLogic.js
+++ gameLogic.js
@@ -1,1 +1,5 @@
-
+//Reurns a random sting of either "rock", "paper" or "scissors"
+function getComputerChoice() {
+  let computerChoice = ["scissors", "rock", "paper"];
+  return computerChoice[Math.floor(Math.random() * 3)];
+}
