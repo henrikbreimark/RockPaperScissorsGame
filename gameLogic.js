940c95c7ecbda35f0ce0ac0d85d4e610d9aea748
Henrik <106661204+henrikbreimark@users.noreply.github.com>
2025-01-10 21:02:38 +0100

    getHumanChoice is working and only returns a valid choice

--- gameLogic.js
+++ gameLogic.js
@@ -3,3 +3,29 @@ function getComputerChoice() {
   let computerChoice = ["scissors", "rock", "paper"];
   return computerChoice[Math.floor(Math.random() * 3)];
 }
+
+//Asks for the players choice and returns in
+function getHumanChoice() {
+  //Will store players choice.
+  let humanChoice;
+
+  //Tells if the choice is a valid choice.
+  let correctInput = false;
+
+  //Loop that continues until the choice is correct. Otherwise it returns information that the choice aren't valid
+  while (correctInput == false) {
+    humanChoice = prompt("Rock, paper or scissors?").toLowerCase();
+    if (
+      humanChoice == "rock" ||
+      humanChoice == "paper" ||
+      humanChoice == "scissors"
+    ) {
+      correctInput = true;
+    } else {
+      console.log("Not a valid choice");
+    }
+  }
+  return humanChoice;
+}
+
+console.log(getHumanChoice());
